842ad23135ce332e9d47d32a33d248c24043fe22
Mudit <mudit.madan@airodigitallabs.com>
2025-07-31 18:45:40 +0530

    added get al documents api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a6f8995347efb7bb"; }
+/******/ 		__webpack_require__.h = function() { return "8f5516c1b98223b9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
